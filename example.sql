943fecaa19a13251bf30e62e8c11b3b70256224f
Gaurang-11 <167474107+Gaurang-11@users.noreply.github.com>
2024-08-21 07:15:55 +0530

    Add files via upload

---
+++ example.sql
@@ -0,0 +1,46 @@
+-- TABLE
+CREATE TABLE customer_orders(
+    order_id integer,
+    customer_id integer,
+    roll_id integer,
+    not_include_items VARCHAR(4),
+    extra_items_included VARCHAR(4),
+    order_date datetime
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE driver_order(
+    order_id integer,
+    driver_id integer,
+    pickup_time datetime,
+    distance VARCHAR(7),
+    duration VARCHAR(10),
+    cancellation VARCHAR(23)
+);
+CREATE TABLE ingredients(
+    ingredients_id integer,
+    ingredients_name varchar(60)
+);
+CREATE TABLE rolls(
+    roll_id integer,
+    roll_name varchar(30)
+);
+CREATE TABLE rolls_recipes(
+    roll_id integer,
+    ingredients varchar(24)
+);
+CREATE TABLE sales_summary(
+    order_id integer,
+    customer_id integer,
+    roll_id integer,
+    base_price decimal(5,2),
+    extra_ingredient_price decimal(5,2),
+    total_price decimal(6,2),
+    order_date datetime
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
